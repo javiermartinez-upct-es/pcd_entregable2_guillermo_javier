91bb746a81bc091e6e792bc0658af3c383c0dae4
“javiermartinez-upct-es” <javier.martinezm@edu.upct.es>
2024-06-23 11:59:49 +0200

    Añadimos metodos de Observer, clase de los datos (timestamp y valor) y Manejador para el calculo de los estadisticos

--- control_de_temperatura_iot.py
+++ control_de_temperatura_iot.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class ServidorSingleton:
     _unicaInstancia = None
@@ -24,7 +24,34 @@ class ServidorSingleton:
         for observador in self.observadores:
             observador.actualizar()
 
+class DatosTemperatura:
+    def __init__(self, timestamp, temperatura):
+        self.timestamp = timestamp
+        self.temperatura = temperatura
 
 class Observador:
+    def __init__(self, sucesor=None):
+        self.sucesor = sucesor
+
+    def manejar_datos(self):
+        if self.sucesor:
+            self.sucesor.manejar_datos()
+
     def actualizar(self):
-        pass
+        self.manejar_datos()
+
+class ManejadorCalculoEstadisticos(Observador):
+    def __init__(self, sucesor=None):
+        super().__init__(sucesor)
+
+    def manejar_datos(self):
+        ahora = datetime.now()
+        hace_60_segundos = ahora - timedelta(seconds=60)
+        datos_recientes = list(filter(lambda x: x.timestamp >= hace_60_segundos, ServidorSingleton.obtenerInstancia().obtener_datos()))
+        temperaturas = list(map(lambda x: x.temperatura, datos_recientes))
+
+        if temperaturas:
+            media = sum(temperaturas) / len(temperaturas)
+            print(f"Media de las temperaturas: {media}")
+
+        super().manejar_datos()
