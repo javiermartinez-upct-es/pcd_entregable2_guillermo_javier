bdb58660f97a97cacde830ed26f986918b8f15d0
“javiermartinez-upct-es” <javier.martinezm@edu.upct.es>
2024-06-23 11:52:14 +0200

    Modificacion metodos de Singleton, y añadimos patrón Observer

--- control_de_temperatura_iot.py
+++ control_de_temperatura_iot.py
@@ -4,6 +4,7 @@ class ServidorSingleton:
     _unicaInstancia = None
 
     def __init__(self):
+        self.observadores = []
         self.datos = []
 
     @classmethod
@@ -15,5 +16,15 @@ class ServidorSingleton:
     def obtener_datos(self):
         return self.datos
 
+    def añadir_observador(self, observador):
+        self.observadores.append(observador)
+
     def notificar(self, datos):
         self.datos.append(datos)
+        for observador in self.observadores:
+            observador.actualizar()
+
+
+class Observador:
+    def actualizar(self):
+        pass
